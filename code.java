d127f2ee71021ef57e89fd787eb6f8b7b3e6b6a1
charanudari17 <charanudari835@gmail.com>
2026-04-13 14:39:32 +0530

    my1t modifcation  example

--- code.java
+++ code.java
@@ -1,5 +1,5 @@
 public class code {
     public static void main(String[] args) {
-        System.err.println("hello");
+        System.out.println("1st modificationin in for git example");
     }
 }
