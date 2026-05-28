c84c6da50e1f943585cc76e48dd8f844606402be
charanudari17 <charanudari835@gmail.com>
2026-05-28 14:29:24 +0530

    rev-string-using recursion

---
+++ revstrrecursion.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+public class revstrrecursion {
+   static String revstr(String s){
+        if(s.length()<=1){
+            return s;
+        }
+        return s.charAt(s.length()-1)+revstr(s.substring(0,s.length()-1));
+    }
+    public static void main(String args[]){
+        String s="123123123";
+        System.out.print(revstr(s));
+
+    }
+    
+}
