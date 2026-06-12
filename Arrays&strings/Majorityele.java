4fa7b54b6ac7e14baa59558702132bc3942c74b0
Udari  Charan <charanudari835@gmail.com>
2026-06-12 20:02:32 +0530

    Improve comments in majorityElement method
    Added comments to clarify the hashmap usage for finding the majority element.

--- Arrays&strings/Majorityele.java
+++ Arrays&strings/Majorityele.java
@@ -3,7 +3,8 @@ import java.util.HashMap;
 public class Majorityele {
  public  int  majorityElement(int[] nums) {
     int ele=-1;
-    //using hashmap
+    //using hashmap 
+ // put all the ele in map with freq --> run a loop on the ele to check the freq if  f is >n/2 return ele
 HashMap<Integer,Integer> map=new HashMap<>();
 int n=nums.length;
 for(int i=0;i<n;i++){
