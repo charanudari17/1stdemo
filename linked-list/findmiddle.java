c50e1f877fb5f6661ebf91ea28bb819d25976c92
charanudari17 <charanudari835@gmail.com>
2026-06-22 10:08:27 +0530

    22-jun

---
+++ linked-list/findmiddle.java
@@ -0,0 +1,18 @@
+class FindMiddle {
+    public FindMiddleNode(ListNode head){
+        if(head==null){
+            return null;
+        }           
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast  !=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+        }
+        return slow;
+        
+    }
+
+
+    }
